84ecf904c701e02a9ea96b3e6bef222c01f33493
fswang <fswang@streamnative.io>
2021-11-11 11:06:27 +0800

    Add pulsar resource apis and controller.

---
+++ api/v1alpha1/pulsartopic_types.go
@@ -0,0 +1,50 @@
+// Copyright (c) 2021 StreamNative, Inc.. All Rights Reserved.
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// PulsarTopicSpec defines the desired state of PulsarTopic
+type PulsarTopicSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of PulsarTopic. Edit pulsartopic_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// PulsarTopicStatus defines the observed state of PulsarTopic
+type PulsarTopicStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// PulsarTopic is the Schema for the pulsartopics API
+type PulsarTopic struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   PulsarTopicSpec   `json:"spec,omitempty"`
+	Status PulsarTopicStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// PulsarTopicList contains a list of PulsarTopic
+type PulsarTopicList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []PulsarTopic `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&PulsarTopic{}, &PulsarTopicList{})
+}
--- main.go
+++ main.go
@@ -1,18 +1,4 @@
-/*
-Copyright 2021.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright (c) 2021 StreamNative, Inc.. All Rights Reserved.
 
 package main
 
@@ -30,6 +16,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	pulsarv1alpha1 "github.com/streamnative/resources-operator/api/v1alpha1"
+	"github.com/streamnative/resources-operator/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +30,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(pulsarv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -74,6 +64,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.PulsarConnectionReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "PulsarConnection")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
