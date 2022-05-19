0ffc5c8487cedf0446514fd164bb23843a58b65a
fswang <fswang@streamnative.io>
2022-05-19 17:14:54 +0800

    fix: remove the space from the company name

--- scripts/setup_git_hook.sh
+++ scripts/setup_git_hook.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-# Copyright 2022 Stream Native
+# Copyright 2022 StreamNative
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
