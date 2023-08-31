f5d556c017874e7d7e4ea68506a05ab1f2a2dcf6
fswang <fswang@streamnative.io>
2023-08-31 17:35:07 +0800

    fix: lint error

--- controllers/pulsarconnection_controller.go
+++ controllers/pulsarconnection_controller.go
@@ -20,11 +20,10 @@ import (
 	"reflect"
 
 	"github.com/go-logr/logr"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
