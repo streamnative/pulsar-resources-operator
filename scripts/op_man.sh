e813314126ef015cbb9005d302189379e379288e
ericsyh <ericshenyuhao@outlook.com>
2023-09-25 16:14:33 +0800

    update
    Signed-off-by: ericsyh <ericshenyuhao@outlook.com>

--- scripts/op_man.sh
+++ scripts/op_man.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2022 StreamNative
+# Copyright 2023 StreamNative
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
