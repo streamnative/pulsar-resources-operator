3c8045139bfd8f3592c311e37afabad35520ad45
Rui Fu <rfu@apache.org>
2026-01-30 15:58:31 +0800

    test(admin): add unit test for OAuth2 issuer URL in PulsarAdmin

--- pkg/admin/interface.go
+++ pkg/admin/interface.go
@@ -30,6 +30,8 @@ import (
 	"github.com/streamnative/pulsar-resources-operator/pkg/utils"
 )
 
+var newAuthenticationOAuth2WithFlow = auth.NewAuthenticationOAuth2WithFlow
+
 // TenantParams indicates the parameters for creating a tenant
 type TenantParams struct {
 	AdminRoles      []string
@@ -419,7 +421,7 @@ func NewPulsarAdmin(conf PulsarAdminConfig) (PulsarAdmin, error) {
 		cfg.KeyFile = keyFilePath
 		cfg.Scope = conf.Scope
 
-		oauthProvider, err := auth.NewAuthenticationOAuth2WithFlow(oauth2.Issuer{
+		oauthProvider, err := newAuthenticationOAuth2WithFlow(oauth2.Issuer{
 			IssuerEndpoint: conf.IssuerEndpoint,
 			ClientID:       conf.ClientID,
 			Audience:       conf.Audience,
